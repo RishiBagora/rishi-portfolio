ac90b56a327581f768fa492bab24a2517f5f77ac
Rishi Bagora <codzenrb@gmail.com>
2026-02-15 15:06:20 +0530

    Update portfolio

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -3,10 +3,12 @@ import MagneticCursor from "../components/MagneticCursor";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import GridOverlay from "../components/GridOverlay";
+import ScrollToTop from "../components/ScrollToTop";
 
 const Layout = () => {
   return (
     <div className="relative min-h-screen z-10">
+      <ScrollToTop/>
         <MagneticCursor/>
       <GridOverlay />
       <Navbar />
